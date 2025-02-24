5519069fd5e51763c91977e2582edfc189f8eb2f
emilykamiti <emilykamiti@gmail.com>
2025-02-24 16:48:19 +0300

    Create Comment rest APi using Postman Client

---
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -0,0 +1,55 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.exception.ResourceNotFoundException;
+import com.springboot.blog.model.Comment;
+import com.springboot.blog.model.Post;
+import com.springboot.blog.payload.CommentDto;
+import com.springboot.blog.repository.CommentRepository;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.CommentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+    private PostRepository postRepository;
+    private CommentRepository commentRepository;
+
+    public CommentServiceImpl(PostRepository postRepository, CommentRepository commentRepository) {
+        this.postRepository = postRepository;
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public CommentDto createComment(long postId, CommentDto commentDto) {
+        Comment comment = mapToEntity(commentDto);
+
+        //retrieve post entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post", "id", postId));
+
+        //set post comment entity
+        comment.setPost(post);
+
+        //comment entity to DB
+        Comment newComment = commentRepository.save(comment);
+        return mapToDto(newComment);
+    }
+
+    private CommentDto mapToDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setBody(comment.getBody());
+        commentDto.setName(comment.getName());
+        commentDto.setEmail(comment.getEmail());
+        return commentDto;
+    }
+
+
+    private Comment mapToEntity(CommentDto commentDto) {
+        Comment comment = new Comment();
+        comment.setId(commentDto.getId());
+        comment.setBody(commentDto.getBody());
+        comment.setName(commentDto.getName());
+        comment.setEmail(commentDto.getEmail());
+        return comment;
+    }
+}
